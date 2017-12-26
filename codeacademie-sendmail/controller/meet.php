b0694d81baf58700232a205609bb482a19fbe5d5
erwannFace <e.duclos@fondationface.org>
2017-12-26 20:04:18 +0100

    Refactoring

--- codeacademie-sendmail/controller/meet.php
+++ codeacademie-sendmail/controller/meet.php
@@ -16,12 +16,12 @@
         .box p {
             font-size: 1.5vh;
         }
-        
+
         .red {
             margin-bottom:2rem;
             color: red;
         }
-        
+
         .green {
             margin-bottom:2rem;
             color: green;
@@ -36,10 +36,18 @@ global $wpdb;
 $table_name = $wpdb->prefix . 'code_academie';
 if (isset($_POST['delete'])){
     $id = $_POST['id_delete'];
-    $result = $wpdb->delete($table_name, array('id' => $id));
+    // $result = $wpdb->delete($table_name, array('id' => $id));
+    $wpdb->update(
+        $table_name,
+        array(
+            'status' => 9
+        ),
+        array( 'id' => $id )
+
+    );
 }
 if(isset($_POST['modif_submit'])){
-    $id = $_POST['id_modif'];    
+    $id = $_POST['id_modif'];
     if (isset($_POST['validate'])){
         if ($_POST['validate'] == "Oui"){
             $status = 1;
@@ -47,9 +55,9 @@ if(isset($_POST['modif_submit'])){
             $status = 0;
         }
     }
-    $wpdb->update( 
-        $table_name, 
-        array( 
+    $wpdb->update(
+        $table_name,
+        array(
             'name' => $_POST['name'],
             'firstname' => $_POST['first_name'],
             'mail' => $_POST['mail'],
@@ -59,34 +67,43 @@ if(isset($_POST['modif_submit'])){
             'name_animal' => $_POST['name_animal'],
             'message' => $_POST['message'],
             'motif' => $_POST['motif'],
-            'status' => $status,
-            'hour_meet' => $_POST['hours']
-        ), 
-        array( 'id' => $id ), 
-        array( 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%s', 
-            '%d', 
-            '%s', 
-        ), 
-        array( '%d' ) 
+            'status' => $_POST['status'],
+            'hour_meet' => $_POST['hour_meet']
+        ),
+        array( 'id' => $id ),
+        array(
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%s',
+            '%d',
+            '%s',
+        ),
+        array( '%d' )
     );
 
 }
+$motifs = [
+  "Vaccin"=> "Vaccin",
+  "Medecine"=> "Médecine",
+  "sterilisation"=> "Chirurgie : stérilisation",
+  "detartrage"=> "Chirurgie : opération",
+  "Autres"=> "Autres",
+];
+$statuses = ["0"=> "Non", "1" => "Oui"];
+
 if (!isset($_POST['modif_form'])){
-    $result = $wpdb->get_results( "SELECT * FROM $table_name WHERE date > CURDATE()  ORDER BY date ASC"); 
-    ?> 
-       <h1>Les prochains rendez-vous : </h1> <?php 
+    $result = $wpdb->get_results( "SELECT * FROM $table_name WHERE date > CURDATE()  ORDER BY date ASC");
+    ?>
+       <h1>Les prochains rendez-vous : </h1> <?php
     foreach ($result as $datas){ ?>
         <div class="box">
-            <h1 class="<?php echo ($datas->hour_meet == null) ? 'red' : 'green' ?>">
+            <h1 class="<?php echo ($datas->status == 0) ? 'red' : 'green' ?>">
                 <?php echo $datas->name . " " . $datas->firstname; ?>
             </h1>
             <div>
@@ -111,10 +128,18 @@ if (!isset($_POST['modif_form'])){
                 </p>
                 <p>Status du rendez vous :
                     <?php
-            if ($datas->status == 0){
-                echo "Non confirmé";
-            } else {
-                echo "Confirmé";
+            switch($datas->status){
+                case 0:
+                  echo "Non confirmé";
+                  break;
+                  case 1:
+                    echo "Confirmé";
+                    break;
+                  case 9:
+                    echo "Supprimé";
+                    break;
+                  default:
+                    echo "Unknown";
             }
             ?>
                 </p>
@@ -146,44 +171,82 @@ $table_name = $wpdb->prefix . 'code_academie';
 $result = $wpdb->get_results( "SELECT * FROM $table_name WHERE id = $id ");
 foreach ($result as $data){}
 ?>
-        <h1>Modifier le rendez-vous</h1>
+  <h1>Modifier le rendez-vous</h1>
+    <section id="rdv">
         <form action="" method="post">
-            <label for="name"><h2>Nom</h2> </label>
+          <div class="section">
+          <div class="">
+
+            <label for="name">Nom :</label>
             <input type="text" name="name" value="<?= $data->name ?>" required>
-            <label for="first_name"><h2>Prénom</h2> </label>
+          </div>
+          <div class="">
+
+            <label for="first_name">Prénom :</label>
             <input type="text" name="first_name" value="<?= $data->firstname ?>" required>
-            <label for="mail"><h2>Adresse Email</h2> </label>
+          </div>
+          <div class="">
+
+            <label for="mail">Adresse Email : </label>
             <input type="email" name="mail" value="<?= $data->mail ?>" required>
-            <label for="tel"><h2>Adresse Email</h2> </label>
+          </div>
+          <div class="">
+
+            <label for="tel">Téléphone :</label>
             <input type="text" name="tel" value="<?= $data->tel ?>" required>
-            <label for="date"> <h2>Date de rendez-vous souhaiter</h2> </label>
+          </div>
+          <div class="">
+
+            <label for="date"> Date de rendez-vous souhaité : </label>
             <input type="date" name="date" value="<?= $data->date ?>" required>
-            <label for="animal"><h2>Espece de l'animal</h2> </label>
+          </div>
+          <div class="">
+
+            <label for="animal">Espèce de l'animal :</label>
             <input type="text" name="animal" value="<?= $data->animal ?>" required>
-            <label for="name_animal"><h2>Nom de l'animal </h2></label>
+          </div>
+          <div class="">
+
+            <label for="name_animal">Nom de l'animal  :</label>
             <input type="text" name="name_animal" value="<?= $data->name_animal ?>" required>
-            <label for="message"><h2>Veuillez entrer ici la raison de votre demande de rendez-vous</h2></label>
+          </div>
+          <div class="">
+
+            <label for="message">Commentaire  :</label>
             <textarea name="message" required><?= $data->message ?></textarea>
-            <?php 
-                if ($data->status == 0){
-                    echo "<p>Confimer le RDV : <input type ='radio' name='validate' value='Oui'>Oui <input type ='radio' name='validate' value='Non' checked='checked'>Non</p>";
-                } else {
-                    echo "<p>Confimer le RDV : <input type ='radio' name='validate' value='Oui' checked='checked'>Oui <input type ='radio' name='validate' value='Non'>Non</p>";
-                } 
-                ?>
+          </div>
+            <div class="">
+
                 <select name="motif">
-                    <option class="motiv"  disabled selected style="display: none;">* Motif</option>
-                    <option value="Vaccin">Vaccin</option>
-                    <option value="Medecine">Médecine</option>
-                    <option value="sterilisation">Chirurgie: stérilisation</option>
-                    <option value="detartrage">Chirurgie: détartrage</option>
-                    <option value="Autres">Autres</option>
+                  <option class="motiv"  disabled selected style="display: none;">* Motif</option>
+                  <?php
+                    foreach($motifs as $key => $val){
+                      echo "<option value='$key' ". ( $data->motif == $key ? 'selected="selected"' : '' ) .">$val</option>";
+
+                    }
+                   ?>
               </select>
-            <label for="hours"><h2>Heure du RDV</h2> </label>
-            <input type="text" name="hours" value="<?php echo ($data->hour_meet == null) ? 'Non définis' : $data->hour_meet ?>" required>
+            </div>
+            <div class="">
+
+            <label for="hours">Heure du RDV </label>
+            <input type="time" name="hour_meet" value="<?php echo ($data->hour_meet == null) ? '00:00' : $data->hour_meet ?>" required>
+          </div>
+            <div class="">
+              <label for="validate">Confirmé : </label>
+              <?php
+
+              foreach ($statuses as $key => $value) {
+                echo "<input type ='radio' name='status' value='$key' ".( $data->status == $key ? "checked='checked'" : "" ).">$value</input>";
+              }
+                  ?>
+            </div>
             <input type="hidden" name="id_modif" value="<?php echo $data->id ?>">
             <button type="submit" name="modif_submit">Modifier rendez-vous</button>
+          </div>
         </form>
+      </section>
+
         <?php } ?>
 
 
